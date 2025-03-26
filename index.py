3e801bb69f2096ca057c655f1bbe499a2d6ced7c
PRDG-k <lasrb6605@naver.com>
2025-03-26 10:47:00 +0900

    test

--- index.py
+++ index.py
@@ -50,12 +50,12 @@
 # print(dog2)
 
 # 1
-name = input("이름을 입력하세요. ")
-age = input("나이를 입력하세요. ")
-print(f"안녕하세요! {name}님 ({age:^5})")
+# name = input("이름을 입력하세요. ")
+# age = input("나이를 입력하세요. ")
+# print(f"안녕하세요! {name}님 ({age:^5})")
 
-# 2
-name = input("이름을 입력하세요. ")
-birth = int(input("태어난 연도를 입력하세요. "))
-year = int(input("올해 연도를 입력하세요. "))
-print(f"올해는 {year}, {name}님의 나이는 {year - birth + 1}세 입니다")
+# # 2
+# name = input("이름을 입력하세요. ")
+# birth = int(input("태어난 연도를 입력하세요. "))
+# year = int(input("올해 연도를 입력하세요. "))
+# print(f"올해는 {year}, {name}님의 나이는 {year - birth + 1}세 입니다")
